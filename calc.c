4f8fa647c1cf151bdd9d58ef4543202bbac827f8
maraki-doc <sdi2500036.di.uoa.gr>
2025-11-04 14:44:14 +0200

    calc

--- calc.c
+++ calc.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 int main() {
     long long int n1,n2, sum;
+    char ans;
     printf("Please enter the first number: ");
     scanf("%lld", &n1);
     printf("Please enter the second number: ");
     scanf("%lld", &n2);
-    sum = n1 + n2;
+    printf("Please enter the equation: ");
+    scanf(" %c", &ans);
+    if (ans == '+'){
+        sum = n1 + n2;
+    }
+    else {
+        sum = n1 - n2;
+    }
     printf("The sum of the two numbers is: %lld\n", sum);
     return 0;
 }
